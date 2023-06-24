adae31eb048e2e1b0a1947107f9410d8e899bb3b
Edward Grunseid <edgru99@gmail.com>
2023-06-24 22:26:51 +0300

    test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const mongose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require('express');
+const app = express();
+const mongose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongose.connect(process.env.DATABASE_ACCESS, () => console.log("database connected"))
+mongose.connect(process.env.DATABASE_ACCESS, () => console.log("database connected"));
 
-app.listen(3000, () => console.log("server is runing"))
+app.listen(3000, () => console.log("server is runing"));
