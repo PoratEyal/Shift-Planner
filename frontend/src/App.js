070b72055260edd8c00427b879df5b2b3e4854c7
eyal porat <eyal1.porat@gmail.com>
2023-07-01 00:08:11 +0300

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import UserManagment from '../src/component/maneger/userManegment/userManagement'
-import Login from '../src/component/all/login'
-import ManagerHomePage from '../src/component/maneger/maneger_home_page'
-<<<<<<< HEAD
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import UserManagement from '../src/component/maneger/userManegment/userManagement';
+import Login from '../src/component/all/login';
+import ManagerHomePage from '../src/component/maneger/maneger_home_page';
 import CreateWeek from '../src/component/maneger/createWeek/createWeek';
-import {Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-=======
-import CreateWeek from './component/maneger/createWeek';
 
->>>>>>> d4adae92279759b7af790f709d3d53e7bd8f656e
 const App = () => {
-
   return <React.Fragment>
-        {/* <Login></Login> */}
-<<<<<<< HEAD
-        <Router>
-          <Routes>
-            <Route path="/" element={<ManagerHomePage />} />
-            <Route path="/userManagment" element={<UserManagment />} />
-            <Route path="/createNewWeek" element={<CreateWeek></CreateWeek>} />
-          </Routes>
-      </Router>
+      {/* <Login></Login> */}
+      
+      <Routes>
+          <Route path="/" element={<ManagerHomePage />} />
+          <Route path="/userManagment" element={<UserManagement />} />
+          <Route path="/createNewWeek" element={<CreateWeek />} />
+      </Routes>
 
-=======
-        
-        <ManagerHomePage></ManagerHomePage> 
-        {/* <UserManagment></UserManagment> */}
-        {/* <CreateWeek></CreateWeek> */}
->>>>>>> d4adae92279759b7af790f709d3d53e7bd8f656e
     </React.Fragment>
-}
+};
 
 export default App;
