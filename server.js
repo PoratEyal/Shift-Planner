091f4cecfb04d338aa01e120c304315ad3764c4a
eyal porat <eyal1.porat@gmail.com>
2023-07-02 14:57:09 +0300

    fg

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// // import area
-// const express = require('express')
-// const app = express()
-// const mongoose = require('mongoose')
-// require('dotenv').config();
-// const routesUrls = require('./routes/routes')
-// const cors = require('cors')
-
-// // database area
-// mongoose.connect(process.env.DATABASE_ACCESS, { useNewUrlParser: true })
-// const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('connected to Database'))
-
-// // app area
-// app.use(express.json())
-// app.use(cors())
-// app.use('/app', routesUrls)
-// app.listen(3001, () => console.log("server is runing"))
-
 // import area
 const express = require('express');
 const app = express();
