95e24d5a42b2bb5a80dc083670a75f5815d256fa
Edward Grunseid <edgru99@gmail.com>
2023-07-02 17:13:18 +0300

    new week scheme

--- models/week.js
+++ models/week.js
@@ -1,24 +1,17 @@
 const mongoose = require('mongoose');
-
 const weekSchema = new mongoose.Schema({
-    weekNumber: {
-      type: Number,
+    name: {
+      type: String,
       required: true
     },
-    // startDate: {
-    //   type: Date,
-    //   required: true,
-    // },
-    // endtDate: {
-    //     type: Date,
-    //     required: true,
-    // },
-    day: {
+    visible: {
+      type: Boolean,
+      default: false 
+    },
+    day: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Day'
-    }
-  });
-
-// Create the user model
+    }]
+});
 const Week = mongoose.model('Week', weekSchema);
 module.exports = Week;
