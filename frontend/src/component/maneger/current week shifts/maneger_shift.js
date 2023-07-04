fffe7fa84016bf2cebda56af90fb367546b83455
eyal porat <eyal1.porat@gmail.com>
2023-07-04 16:14:01 +0300

    כעכע

--- frontend/src/component/maneger/current week shifts/maneger_shift.js
+++ frontend/src/component/maneger/current week shifts/maneger_shift.js
@@ -6,12 +6,60 @@ import ManegerWorkerList from '../current week shifts/maneger_workerList'
 const MamegerShifts = (props) => {
 
     const [shift, setShift] = useState(props.shift);
-    const data = JSON.parse(localStorage.getItem("user"));
+    //const data = JSON.parse(localStorage.getItem("user"));
+
+    const addWorkerShift = (workerId) => {
+        const index = shift.availableWorkers.findIndex(x => x === workerId)
+        const arrTmp = [...shift.availableWorkers]
+        arrTmp.splice(index, 1)
+        console.log(arrTmp)
+        const newShift = {
+            _id: shift._id,
+            description: shift.description,
+            startTime: shift.startTime,
+            endTime: shift.endTime,
+            workers: [...shift.workers, workerId],
+            availableWorkers: arrTmp
+        }
+        try {
+            axios.put("http://localhost:3001/app/updateShift", newShift)
+            .then((response) => {
+                console.log(response.data)
+                window.location.reload();
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+
+    const removeWorkerShift = (workerId) => {
+        const index = shift.workers.findIndex(x => x === workerId)
+        const arrTmp = [...shift.workers]
+        arrTmp.splice(index, 1)
+        const newShift = {
+            _id: shift._id,
+            description: shift.description,
+            startTime: shift.startTime,
+            endTime: shift.endTime,
+            workers: arrTmp,
+            availableWorkers: [...shift.availableWorkers, workerId]
+        }
+        try {
+            axios.put("http://localhost:3001/app/updateShift", newShift)
+            .then((response) => {
+                console.log(response.data)
+                window.location.reload();
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return <div className={styles.shift}>
         <p className={styles.shift_name}>{shift.description}</p>
         <p>משעה - {shift.startTime} עד {shift.endTime}</p>
-        <ManegerWorkerList workers={shift.availableWorkers}></ManegerWorkerList>
+        <ManegerWorkerList removeWorkerShift={removeWorkerShift} addWorkerShift={addWorkerShift} workers={shift.workers} availableWorkers={shift.availableWorkers}></ManegerWorkerList>
     </div>
 }
 
--- frontend/src/component/maneger/current week shifts/maneger_workerList.js
+++ frontend/src/component/maneger/current week shifts/maneger_workerList.js
@@ -5,17 +5,21 @@ import styles from '../createWeek/createWeek.module.css'
 const ManegerWorkerList = (props) => {
     
     const [workers, setWorkers] = useState(props.workers)
-    const [workerNames, setWorkerNames] = useState([]);
-    const [loading, setLoading] = useState(false)
-    const [chosen, setChosen] = useState('בחירה')
+    const [availableWorkers, setAvailableWorkers] = useState(props.availableWorkers)
 
+    const [availableWorkersArr, setAvailableWorkersArr] = useState([]);
+    const [workersArr, setWorkersArr] = useState([]);
+
+    const [loading, setLoading] = useState(false)
+
+    // get all the workers
     useEffect(() => {
-        workers.map(worker => {
+      workers.map(worker => {
             axios.get(`http://localhost:3001/app/getUserById/${worker}`)
                 .then(response => {
                     setLoading(true)
-                    const fullName = response.data.fullName;
-                    setWorkerNames(prevWorkerNames => [...prevWorkerNames, fullName]);
+                    const worker = response.data;
+                    setWorkersArr(prevWorkerNames => [...prevWorkerNames, worker]);
                 })
                 .catch(error => {
                     console.error(error);
@@ -24,30 +28,51 @@ const ManegerWorkerList = (props) => {
         setLoading(true)
     }, []);
 
-    const choseWorker = (fullname) => {
-      console.log(fullname)
+    // get all the available workers
+    useEffect(() => {
+      availableWorkers.map(worker => {
+            axios.get(`http://localhost:3001/app/getUserById/${worker}`)
+                .then(response => {
+                    setLoading(true)
+                    const worker = response.data;
+                    setAvailableWorkersArr([...availableWorkersArr, worker])
+                  })
+                .catch(error => {
+                    console.error(error);
+                });
+        });
+        setLoading(true)
+    }, []);
+
+    const choseWorker = (id) => {
+      console.log(id)
+      props.addWorkerShift(id)
+    }
+
+    const removeWorker = (id) => {
+      console.log(id)
+      props.removeWorkerShift(id)
     }
     
     return (
-        <div className={styles.workers_list_delete}>
-          {!loading ? (
-            <div className={styles.loadingWorkers_manger_page}>
-              <div className={styles['three-body']}>
-                <div className={styles['three-body__dot']}></div>
-                <div className={styles['three-body__dot']}></div>
-                <div className={styles['three-body__dot']}></div>
-              </div>
+      <div className={styles.workers_list_delete}>
+
+        {workersArr.map((worker, index) => (
+            <div key={index} className={styles.nameAndDelete}>
+              <button onClick={() => removeWorker(worker._id)} className={styles.btn_chosen}>הסר</button>
+              <p className={styles.names}>{worker.fullName}</p>
             </div>
-          ) : (
-            workerNames.map((name, index) => (
-              <div key={index} className={styles.nameAndDelete}>
-                <button onClick={() => choseWorker(name)} className={styles.btn_chose}>{chosen}</button>
-                <p className={styles.names}>{name}</p>
-              </div>
-            ))
-          )}
-        </div>
-      );
+          ))}
+
+          {availableWorkersArr.map((worker, index) => (
+            <div key={index} className={styles.nameAndDelete}>
+              <button onClick={() => choseWorker(worker._id)} className={styles.btn_chose}>בחירה</button>
+              <p className={styles.names}>{worker.fullName}</p>
+            </div>
+          ))}
+
+      </div>
+    );
       
 }
 
