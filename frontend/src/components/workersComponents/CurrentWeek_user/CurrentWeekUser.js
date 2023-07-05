dd17d82b7d728589d1287536e0d0ba201809450d
eyal porat <eyal1.porat@gmail.com>
2023-07-05 18:53:00 +0300

    גכגכ

--- frontend/src/components/workersComponents/CurrentWeek_user/CurrentWeekUser.js
+++ frontend/src/components/workersComponents/CurrentWeek_user/CurrentWeekUser.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Day from './CurrentDayser'
+import styles from './currentWeekUser.module.css';
 
 const CurrentWeekUser = () => {
 
     const[week, setWeek] = useState(null);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const getDays = () => {
              axios.get("http://localhost:3001/app/getCurrentWeek").then((response) => {
@@ -18,8 +20,19 @@ const CurrentWeekUser = () => {
 
     }, []);
 
+    const handleMenuClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return <React.Fragment>
-        <div>
+        <div className={styles.all}>
+
+        <div className={`${styles.containerMenu} ${isMenuOpen ? styles.change : ''}`} onClick={handleMenuClick}>
+            <div className={styles.bar1}></div>
+            <div className={styles.bar2}></div>
+            <div className={styles.bar3}></div>
+        </div>
+
             {
                week ?  week.day.map((day) => {
                     return <Day day={day} key={day._id}></Day>
