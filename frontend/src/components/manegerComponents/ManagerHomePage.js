20a2495bf5577e4625f2b68e7c6da08c56797c95
eyal porat <eyal1.porat@gmail.com>
2023-07-08 20:58:06 +0300

    עהיכעי

--- frontend/src/components/manegerComponents/ManagerHomePage.js
+++ frontend/src/components/manegerComponents/ManagerHomePage.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, Outlet} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { BiLogOut } from "react-icons/bi";
+import { BiUserCircle } from "react-icons/bi";
 
 const ManagerHomePage = () => {
     const navigate = useNavigate();
@@ -21,10 +22,14 @@ const ManagerHomePage = () => {
     }, [])
 
 
-    return <div className={styles.all}>
-        
+    return <React.Fragment>
         <div className={styles.upperContainer}>
-            <Link to="/"><button className={styles.signout} onClick={() => {localStorage.clear()}}><BiLogOut></BiLogOut></button></Link>
+            <div className={styles.nav_buttons}>
+                <Link to="/"><button className={styles.signout} onClick={() => {localStorage.clear()}}><BiLogOut></BiLogOut></button></Link>
+                <Link to="/managerSettings"><button className={styles.user_settings}><BiUserCircle></BiUserCircle></button></Link>
+                <Link to="/userManagment"><button className={styles.user_managment_btn}>ניהול עובדים</button></Link>
+            </div>
+
             <h1 className={styles.h1}>שלום {fullname}</h1>
         </div>
 
@@ -36,11 +41,10 @@ const ManagerHomePage = () => {
             <button className={styles.btn}>בחירת עובדים לשבוע הבא</button>
             </Link>
             <Link to="/createNewWeek"><button className={styles.btn}>בנית משמרות לשבוע הבא</button></Link> 
-            <Link to="/userManagment"><button className={styles.btn1}>ניהול עובדים</button></Link>
-            <Link to='/managerSettings'><button className={styles.btn1}>ערוך משתמש</button></Link>
+
             <Outlet />
         </div>
-    </div>
+    </React.Fragment>
 }
 
 export default ManagerHomePage;
