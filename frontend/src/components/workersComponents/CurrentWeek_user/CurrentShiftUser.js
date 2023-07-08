b333f73d765e8baa00813ad8a2f1977a9d4ea8bf
Edward Grunseid <edgru99@gmail.com>
2023-07-08 18:00:41 +0300

    show Workers

--- frontend/src/components/workersComponents/CurrentWeek_user/CurrentShiftUser.js
+++ frontend/src/components/workersComponents/CurrentWeek_user/CurrentShiftUser.js
@@ -8,22 +8,23 @@ const CurrentShiftUser = (props) => {
     const [shift] = useState(props.shift);
     const [userId, setUserId] = useState("");
     const [addClass, setAddClass] = useState(false)
-
+    const [showWorkers, setShow] = useState(false);
     useEffect(() => {
         const newdata = JSON.parse(localStorage.getItem("user"));
         setUserId(newdata._id);
     
         if (shift.workers.includes(newdata._id)) {
             setAddClass(true);
+            setShow(true);
         } else {
             setAddClass(false);
         }
     }, [shift, userId]);
 
     return (
-        <div className={`${styles.shift} ${addClass ? styles.worksHer : ''}`}>
+        <div className={`${styles.shift} ${addClass ? styles.worksHer : ''}`} onClick={() => {setShow(!showWorkers)}}>
             <p className={styles.shift_name}>{shift.description}: {shift.startTime} - {shift.endTime}</p>
-            <WorkersCurrentWeek workers={shift.workers}></WorkersCurrentWeek>
+            {showWorkers ? <WorkersCurrentWeek workers={shift.workers}></WorkersCurrentWeek> : null}
             {/* {addClass ? <h3><BiSolidWinkSmile className={styles.icon}></BiSolidWinkSmile><label>&nbsp;משמרת נעימה</label></h3> : null} */}
         </div>
     );
