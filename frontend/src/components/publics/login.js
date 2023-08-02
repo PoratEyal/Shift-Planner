75efa2be907c00baba02d63293d5aeaa32840492
eyal porat <eyal1.porat@gmail.com>
2023-08-02 18:26:35 +0300

    dded hide and show password

--- frontend/src/components/publics/login.js
+++ frontend/src/components/publics/login.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Roles from './Roles';
-
 import styles from '../publics/login.module.css';
+import { BiSolidShow, BiSolidHide } from "react-icons/bi";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [notValid, setNotValid] = useState(false);
-  const [validationKey, setValidationKey] = useState(0); // State variable for unique key
+  const [validationKey, setValidationKey] = useState(0);
+  const [show, setShow] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,7 +21,6 @@ const Login = () => {
     }
   }, []);
 
-  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.post(`${process.env.REACT_APP_URL}/login`, { username: username, password: password }).then((response) => {
@@ -32,7 +32,7 @@ const Login = () => {
         Roles.checkUserRole(user.job) ? navigate('/managerHomePage') : navigate('/CurrentWeek');
       } else {
         setNotValid(true);
-        setValidationKey(prevKey => prevKey + 1); // Update the key to trigger re-render
+        setValidationKey(prevKey => prevKey + 1);
       }
     }).catch(() => {
     });
@@ -40,22 +40,38 @@ const Login = () => {
 
   return (
     <div className={styles.container}>
+
       <div className={styles.logo}></div>
+
       <div className={styles.container_div}>
         <form className={styles.form} onSubmit={handleSubmit}>
+
           <div>
             <input type="text" placeholder='שם משתמש' className={styles.input} onChange={(e) => { setUsername(e.target.value) }} />
           </div>
-          <div>
-            <input type="password" placeholder='סיסמה' className={styles.input} onChange={(e) => { setPassword(e.target.value) }} />
+
+          <div className={styles.password_div}>
+            <div className={styles.input_container}>
+              <input id='password' type={show ? "password" : "text"} placeholder='סיסמה' className={styles.input} onChange={(e) => { setPassword(e.target.value) }} />
+                {show ? 
+                  <BiSolidShow className={styles.show_password} onClick={(e) => setShow(!show)}></BiSolidShow>
+                  :
+                  <BiSolidHide className={styles.show_password} onClick={(e) => setShow(!show)}></BiSolidHide>
+                }
+            </div>
           </div>
+
           {
             notValid ? <p key={validationKey} className={styles.validation_p}>שם משתמש או סיסמא שגויים</p> : null
           }
+
           <button className={styles.btn} type="submit">התחברות</button>
+
           <div className={styles.blueBack}></div>
+
         </form>
       </div>
+
     </div>
   );
 };
