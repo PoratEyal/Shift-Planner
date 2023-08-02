841f37662713800f2d04cab340c46fbe802a6c66
eyal porat <eyal1.porat@gmail.com>
2023-08-02 16:17:32 +0300

    ui changes for login page

--- frontend/src/components/publics/login.js
+++ frontend/src/components/publics/login.js
@@ -1,13 +1,15 @@
-import styles from '../publics/login.module.css'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Roles from './Roles';
 
+import styles from '../publics/login.module.css';
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [notValid, setNotValid] = useState(false);
+  const [validationKey, setValidationKey] = useState(0); // State variable for unique key
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,72 +20,44 @@ const Login = () => {
     }
   }, []);
 
-  return <div className={styles.container}>
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`${process.env.REACT_APP_URL}/login`, { username: username, password: password }).then((response) => {
+      if (response.status === 200) {
+        const user = response.data;
+        localStorage.setItem("token", user.token);
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("isAuth", true);
+        Roles.checkUserRole(user.job) ? navigate('/managerHomePage') : navigate('/CurrentWeek');
+      } else {
+        setNotValid(true);
+        setValidationKey(prevKey => prevKey + 1); // Update the key to trigger re-render
+      }
+    }).catch(() => {
+    });
+  };
 
-    <div className={styles.logo}></div>
-
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}></div>
       <div className={styles.container_div}>
-
-        <form className={styles.form} onSubmit={(e) => {
-          e.preventDefault();
-          axios.post(`${process.env.REACT_APP_URL}/login`, { username: username, password: password }).then((response) => {
-            if(response.status === 200){
-            const user = response.data;
-            localStorage.setItem("token", user.token);
-            localStorage.setItem("user", JSON.stringify(user));
-            localStorage.setItem("isAuth", true);
-            Roles.checkUserRole(user.job) ? navigate('/managerHomePage') : navigate('/CurrentWeek');
-            }
-            else{
-              setNotValid(true);
-            }
-
-          }).catch(() => {
-          })
-        }}>
-
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div>
-            <div className={styles["wave-group"]}>
-              <input required type="text" className={styles.input} onChange={(e) => { setUsername(e.target.value) }} />
-              <span className={styles.bar}></span>
-              <label className={styles.label}>
-                <span className={styles["label-char"]} style={{ "--index": 7 }}>ש</span>
-                <span className={styles["label-char"]} style={{ "--index": 6 }}>מ</span>
-                <span className={styles["label-char"]} style={{ "--index": 5 }}>ת</span>
-                <span className={styles["label-char"]} style={{ "--index": 4 }}>ש</span>
-                <span className={styles["label-char"]} style={{ "--index": 3 }}>מ</span>
-                <span className={styles["label-char"]} style={{ "--index": 2 }}>&nbsp;</span>
-                <span className={styles["label-char"]} style={{ "--index": 1 }}>ם</span>
-                <span className={styles["label-char"]} style={{ "--index": 0 }}>ש</span>
-              </label>
-            </div>
+            <input type="text" placeholder='שם משתמש' className={styles.input} onChange={(e) => { setUsername(e.target.value) }} />
           </div>
-
           <div>
-            <div className={styles["wave-group"]}>
-              <input required type="password" className={styles.input} onChange={(e) => { setPassword(e.target.value) }} />
-              <span className={styles.bar}></span>
-              <label className={styles.label}>
-                <span className={styles["label-char"]} style={{ "--index": 4 }}>ה</span>
-                <span className={styles["label-char"]} style={{ "--index": 3 }}>מ</span>
-                <span className={styles["label-char"]} style={{ "--index": 2 }}>ס</span>
-                <span className={styles["label-char"]} style={{ "--index": 1 }}>י</span>
-                <span className={styles["label-char"]} style={{ "--index": 0 }}>ס</span>
-              </label>
-            </div>
+            <input type="password" placeholder='סיסמה' className={styles.input} onChange={(e) => { setPassword(e.target.value) }} />
           </div>
           {
-            notValid ? <p className={styles.validation_p}>שם משתמש או סיסמא שגויים</p> : null
+            notValid ? <p key={validationKey} className={styles.validation_p}>שם משתמש או סיסמא שגויים</p> : null
           }
-          
           <button className={styles.btn} type="submit">התחברות</button>
-
           <div className={styles.blueBack}></div>
-          
         </form>
-
       </div>
     </div>
-}
+  );
+};
 
 export default Login;
