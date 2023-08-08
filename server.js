5ae157a0b09fef8724cf2babc75b50be56c1d791
Edward Grunseid <edgru99@gmail.com>
2023-08-08 19:58:27 +0300

    f

--- server.js
+++ server.js
@@ -28,10 +28,12 @@ db.once('open', () => console.log('connected to Database...'))
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 var allowedDomains = process.env.ALLOWED_DOMAINS.split(", ");
+console.log(allowedDomains);
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin) return callback(null, true);
-        if (allowedDomains.indexOf(origin) === -1) {
+        console.log(origin);
+        if (!allowedDomains.includes(origin)) {
           var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
           return callback(new Error(msg), false);
         }
