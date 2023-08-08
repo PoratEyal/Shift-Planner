21a67fb99065f1cd510b6f3361d4f069da115892
Edward Grunseid <edgru99@gmail.com>
2023-08-08 16:52:24 +0300

    CHANGES

--- models/day.js
+++ models/day.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const Shift = require('./shift');
 const daySchema = new mongoose.Schema({
+    date: {
+      type: Date,
+      required: true
+    },
+    holiday: {
+      type: String
+    },
     name: {
       type: String,
       required: true
--- utils/functions.js
+++ utils/functions.js
@@ -3,23 +3,31 @@ const Week = require('../models/week');
 const Day = require('../models/day');
 const User = require('../models/user');
 const { response } = require('express');
-function CreateWeek(){
-    User.find({manager: {$exists: false}}).exec().then(documents => {
-      documents.forEach(document => {
-        const id = document._id;
-        const days = ['ראשון', 'שני', "שלישי", "רביעי", "חמישי", "שישי", "שבת"];
-        Week.create({ name: 'NextWeek', visible: false, publishScheduling:false, ofManager: id, day: [] })
+function CreateWeek() {
+  User.find({ manager: { $exists: false } }).exec().then(documents => {
+    documents.forEach(document => {
+      const id = document._id;
+      const days = ['ראשון', 'שני', "שלישי", "רביעי", "חמישי", "שישי", "שבת"];
+      Week.create({ name: 'NextWeek', visible: false, publishScheduling: false, ofManager: id, day: [] })
         .then(newWeek => {
+
+
+          const currentDate = new Date();
+          
+
+
           const saveNextDay = index => {
             if (index >= days.length) {
               return newWeek.save();
             }
             const day = days[index];
-            return Day.create({ name: day, shifts: [] })
+            const futureDate = new Date(currentDate);
+            futureDate.setDate(currentDate.getDate() + index);
+            return Day.create({ name: day, shifts: [] , date: futureDate})
               .then(newDay => {
                 newWeek.day.push(newDay);
                 return newDay.save().then(() => {
-                    saveNextDay(index + 1);
+                  saveNextDay(index + 1);
                 });
               });
           };
@@ -28,8 +36,8 @@ function CreateWeek(){
         .catch(error => {
           console.error(error);
         });
-      });
     });
+  });
 }
 
 module.exports = CreateWeek
