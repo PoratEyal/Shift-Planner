c52e33ac6b74f39def9c678a1bfe955a407697ba
eyal porat <eyal1.porat@gmail.com>
2023-08-19 21:06:19 +0300

    ui changes

--- frontend/src/components/manegerComponents/ManagerHomePage.js
+++ frontend/src/components/manegerComponents/ManagerHomePage.js
@@ -92,7 +92,7 @@ const ManagerHomePage = () => {
 
                 <div className={styles.spacer}></div>
 
-                <div>
+                <div className={styles.nav_btn_div}>
                     <Link to="/managerSettings"><button><BiUserCircle></BiUserCircle></button></Link>
                     <Link to="/"><button onClick={signout}><BiLogOut></BiLogOut></button></Link>   
                 </div>
