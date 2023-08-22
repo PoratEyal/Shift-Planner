4b1c48be0c32a94f197052f79361c59558542bb3
Edward Grunseid <edgru99@gmail.com>
2023-08-22 15:20:29 +0300

    changes

--- routes/gpt.routes.js
+++ routes/gpt.routes.js
@@ -11,8 +11,8 @@ gptApiRouter.use(bodyParser.json());
 
 gptApiRouter.post('/sendMessegeAPI', async (req, res) => {
               
-  const { messages } = req.body.messages;
-  const id = req.body.id;
+  const { messages } = req.body;
+  //const id = req.body.id;
   console.log(`id: ${id} made a request `)
   const options = {
     method: 'POST',
@@ -29,7 +29,7 @@ gptApiRouter.post('/sendMessegeAPI', async (req, res) => {
 
   try {
     const response = await axios(options);
-    console.log(`got response for id: ${id}`);
+    console.log(`got response for id: `);
     const data = response.data;
     const answerResponse = data.choices[0].message.content;
     res.json(answerResponse);
