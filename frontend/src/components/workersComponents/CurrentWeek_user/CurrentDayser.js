d3c99567d218bbf3d0edfafa642033d2e911dd40
eyal porat <eyal1.porat@gmail.com>
2023-08-23 19:41:48 +0300

    add scroll

--- frontend/src/components/workersComponents/CurrentWeek_user/CurrentDayser.js
+++ frontend/src/components/workersComponents/CurrentWeek_user/CurrentDayser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './currentWeekUser.module.css';
 import Shift from './CurrentShiftUser';
 import moment from "moment";
@@ -7,9 +7,22 @@ const CurrentDayUser = (props) => {
 
   const [day] = useState(props.day);
 
+  useEffect(() => {
+    const today = moment().format('YYYY-MM-DD');
+    console.log(today)
+    if (moment(day.date).format('YYYY-MM-DD') === today) {
+      // Scroll to the day's container element
+      const dayContainer = document.getElementById(`day_${day.date}`);
+      console.log(dayContainer)
+      if (dayContainer) {
+        dayContainer.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [day.date]);
+
   return (
     <div>
-      <div className={styles.day_container}>
+      <div className={styles.day_container} id={`day_${day.date}`}>
         
         <h2 className={styles.h2}>{day.name} - {moment(day.date).format('DD.MM')}</h2>
 
