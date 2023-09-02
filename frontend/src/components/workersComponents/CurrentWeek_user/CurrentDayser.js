138199abfbea71fb6978cd0763eb03a6e2a34013
Edward Grunseid <edgru99@gmail.com>
2023-09-03 00:07:27 +0300

    worker utc time fixed

--- frontend/src/components/workersComponents/CurrentWeek_user/CurrentDayser.js
+++ frontend/src/components/workersComponents/CurrentWeek_user/CurrentDayser.js
@@ -30,7 +30,7 @@ const CurrentDayUser = (props) => {
     <div>
       <div className={styles.day_container} id={`day_${day.date}`}>
         
-        <h2 className={styles.h2}>{day.name} - {moment(day.date).format('DD.MM')}</h2>
+        <h2 className={styles.h2}>{day.name} - {moment(day.date).utc().format('DD.MM')}</h2>
 
         {day.shifts?.length === 0 ? (
           <div className={styles.no_shifts_message}>אין משמרות ליום זה</div>
