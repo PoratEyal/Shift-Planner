500b6dd98383ae253cc4c255462286b12422c4b2
Edward Grunseid <edgru99@gmail.com>
2023-09-12 12:00:42 +0300

    fixed bugs

--- frontend/src/components/manegerComponents/EditCurrentWeek/EditDayCurrentWeek.js
+++ frontend/src/components/manegerComponents/EditCurrentWeek/EditDayCurrentWeek.js
@@ -74,7 +74,7 @@ const EditDayCurrentWeek = (props) => {
 
     return <div className={`${styles.day_container}`} style={{ margin: '12px' }} id={`day_${day.date}`}>
 
-            <h2 className={styles.h2}>{day.name} - {moment(day.date).format('DD.MM')}</h2>
+            <h2 className={styles.h2}>{day.name} - {moment(day.date).utc().format('DD.MM')}</h2>
             {
                 loading ? (
                     <div className={styles['three-body']}>
--- utils/CreateWeekTest.js
+++ utils/CreateWeekTest.js
@@ -1,30 +1,49 @@
 const Week = require('../models/week');
 const Day = require('../models/day');
+const User = require('../models/user');
 
-async function createWeekForManager(managerId) {
-  const days = ['ראשון', 'שני', "שלישי", "רביעי", "חמישי", "שישי", "שבת"];
-  try {
-    const newWeek = await Week.create({ name: 'NextWeek', visible: false, publishScheduling: false, ofManager: managerId, usedAi: false, day: [] });
+const { response } = require('express');
+const { DateTime } = require('luxon')
+async function createWeekForManager() {
+  User.find({ manager: { $exists: false } }).exec().then(documents => {
+    documents.forEach(document => {
+      const id = document._id;
+      const israelTimezone = 'Asia/Jerusalem'; 
+      const currentDateInIsrael = DateTime.now().setZone(israelTimezone);
 
-    const currentDate = new Date();
-    const futureDate = new Date(currentDate);
-    
-    async function saveNextDay(index) {
-      if (index >= days.length) {
-        return newWeek.save();
-      }
-      futureDate.setDate(currentDate.getDate() + (index+7));
-      const day = days[index];
-      const newDay = await Day.create({ name: day, shifts: [], date: futureDate});
-      newWeek.day.push(newDay);
-      await newDay.save();
-      return saveNextDay(index + 1);
-    }
-
-    await saveNextDay(0);
-  } catch (error) {
-    console.error(error);
-  }
+      const days = ['ראשון', 'שני', "שלישי", "רביעי", "חמישי", "שישי", "שבת"];
+      Week.create({ name: 'NextWeek', visible: false, publishScheduling: false, ofManager: id, usedAi: false, day: [] })
+        .then(newWeek => {
+          const saveNextDay = index => {
+            if (index >= days.length) {
+              return newWeek.save();
+            }
+            const day = days[index];
+            const futureDate = new Date(
+              currentDateInIsrael.year,
+              currentDateInIsrael.month - 1, 
+              currentDateInIsrael.day + (index +7),
+              currentDateInIsrael.hour,
+              currentDateInIsrael.minute,
+              currentDateInIsrael.second
+            );
+            futureDate.setMinutes(futureDate.getMinutes() + currentDateInIsrael.offset);
+            
+            return Day.create({ name: day, shifts: [], date: futureDate})
+              .then(newDay => {
+                newWeek.day.push(newDay);
+                return newDay.save().then(() => {
+                  saveNextDay(index + 1);
+                });
+              });
+          };
+          return saveNextDay(0);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
+  });
 }
 
 module.exports = createWeekForManager;
