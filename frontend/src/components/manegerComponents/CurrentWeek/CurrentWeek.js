5242dd1432221304a1b00ec09c1920e3f2ff2c69
eyal porat <eyal1.porat@gmail.com>
2023-09-27 18:01:07 +0300

    ui changes

--- frontend/src/components/manegerComponents/CurrentWeek/CurrentWeek.js
+++ frontend/src/components/manegerComponents/CurrentWeek/CurrentWeek.js
@@ -359,10 +359,15 @@ const CurrentWeek = () => {
                     <div className={styles.loader}>
                         <div className={styles.words}>
                             <span className={styles.word}></span>
-                            <span className={styles.word}>..טוען משמרות</span>
-                            <span className={styles.word}>..טוען עובדים</span>
-                            <span className={styles.word}>..בודק בקשות</span>
-                            <span className={styles.word}>..מחשב שיבוצים</span>
+                            <span className={styles.word}>טוען שבוע</span>
+                            <span className={styles.word}>טוען ימים</span>
+                            <span className={styles.word}>טוען משמרות</span>
+                            <span className={styles.word}>טוען עובדים</span>
+                            <span className={styles.word}>טוען בקשות עובדים</span>
+                            <span className={styles.word}>מאמת נתונים</span>
+                            <span className={styles.word}>מחשב שיבוצים</span>
+                            <span className={styles.word}>מסכם תוצאות</span>
+                            <span className={styles.word}>בודק תקינות</span>
                         </div>
                     </div>
                 </div>: null}
