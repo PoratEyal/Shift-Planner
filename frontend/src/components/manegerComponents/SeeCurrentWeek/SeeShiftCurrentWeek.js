241dd16e1e30d0445014659fdc35a288bcb76164
eyal porat <eyal1.porat@gmail.com>
2024-02-08 19:46:47 +0200

    ui changes

--- frontend/src/components/manegerComponents/SeeCurrentWeek/SeeShiftCurrentWeek.js
+++ frontend/src/components/manegerComponents/SeeCurrentWeek/SeeShiftCurrentWeek.js
@@ -14,17 +14,21 @@ const SeeShiftCurrentWeek = (props) => {
           <p className={styles.shift_description}>
             {shift.description}&nbsp;: {moment(shift.endTime).format('HH:mm')} - {moment(shift.startTime).format('HH:mm')}
             
-            <label  className={styles.icon_container}>
-              <input type="checkbox" onClick={() => {setShow(!showWorkers)}}></input >
-              <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" className={styles.chevron_down}>
-                <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
-              </svg>
-            </label>
-            
-            {/* {!showWorkers ? 
-              <FcPrevious className={styles.under_icon}></FcPrevious>
-            : 
-              <FcExpand className={styles.under_icon}></FcExpand>} */}
+            {showWorkers ? 
+                <label className={styles.icon_container}>
+                    <input checked type="checkbox" onClick={() => {setShow(!showWorkers)}}></input >
+                    <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" className={styles.chevron_down}>
+                        <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
+                    </svg>
+                </label>
+                :
+                <label className={styles.icon_container}>
+                    <input type="checkbox" onClick={() => {setShow(!showWorkers)}}></input >
+                    <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" className={styles.chevron_down}>
+                        <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
+                    </svg>
+                </label>}
+
           </p>
         { showWorkers ?<CurrentWeekWorkers managerId={props.managerId} standBy={shift.standBy} workers={shift.workers} shiftData={shift.shiftData} endTime={shift.endTime} startTime={shift.startTime}></CurrentWeekWorkers> : null }
     </div>
--- frontend/src/components/workersComponents/chooseShifts/UserShift.js
+++ frontend/src/components/workersComponents/chooseShifts/UserShift.js
@@ -75,14 +75,21 @@ const UserShift = (props) => {
           <p className={styles.shift_data_published}>
             {shift.description}&nbsp;: {moment(shift.endTime).format('HH:mm')} - {moment(shift.startTime).format('HH:mm')}
 
-            <label className={styles.icon_container}>
-                <input type="checkbox" onClick={() => {setShow(!showWorkers)}}></input >
-                <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" className={styles.chevron_down}>
-                    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
-                </svg>
-            </label>
+            {showWorkers ? 
+                <label className={styles.icon_container}>
+                    <input checked type="checkbox" onClick={() => {setShow(!showWorkers)}}></input >
+                    <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" className={styles.chevron_down}>
+                        <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
+                    </svg>
+                </label>
+                :
+                <label className={styles.icon_container}>
+                    <input type="checkbox" onClick={() => {setShow(!showWorkers)}}></input >
+                    <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" className={styles.chevron_down}>
+                        <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
+                    </svg>
+                </label>}
 
-            {/* {showWorkers ? <FcExpand className={styles.under_icon} /> : <FcPrevious className={styles.under_icon} />} */}
           </p>
         </div>
       )}
