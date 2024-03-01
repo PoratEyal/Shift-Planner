985c27244af82502a9e3ae072f5c0bf62257ce5e
eyal porat <eyal1.porat@gmail.com>
2024-03-02 00:19:35 +0200

    ui changes

--- frontend/src/components/publics/OTP/otp.js
+++ frontend/src/components/publics/OTP/otp.js
@@ -30,6 +30,13 @@ const Otp = (props) => {
         })    
     }
 
+    // Function to focus on the next input field
+    const focusNextInput = (currentInput, nextInput) => {
+        if (currentInput.current.value.length === 1) {
+            nextInput.current.focus();
+        }
+    };
+
     return <form className={styles.container} onSubmit={(e) => handleSubmit(e)}>
         
         <IoArrowBack onClick={() => props.setOpenPopUp(false)} className={styles.backIcon}></IoArrowBack>
@@ -48,10 +55,10 @@ const Otp = (props) => {
 
         <div className={styles.middle_div}>
             <div className={styles.input_div}>
-                <input className={styles.input} required maxLength="1" type="text" id="otp-input1" ref={otp_input1} />
-                <input className={styles.input} required maxLength="1" type="text" id="otp-input2" ref={otp_input2}/>
-                <input className={styles.input} required maxLength="1" type="text" id="otp-input3" ref={otp_input3}/>
-                <input className={styles.input} required maxLength="1" type="text" id="otp-input4" ref={otp_input4}/>
+                <input className={styles.input} required maxLength="1" type="text" id="otp-input1" ref={otp_input1} onInput={() => focusNextInput(otp_input1, otp_input2)} />
+                <input className={styles.input} required maxLength="1" type="text" id="otp-input2" ref={otp_input2} onInput={() => focusNextInput(otp_input2, otp_input3)} />
+                <input className={styles.input} required maxLength="1" type="text" id="otp-input3" ref={otp_input3} onInput={() => focusNextInput(otp_input3, otp_input4)} />
+                <input className={styles.input} required maxLength="1" type="text" id="otp-input4" ref={otp_input4} />
             </div>
         </div>
 
