7d191ea6bc09a5414802bf8c63c61f0151a39dfc
eyal porat <eyal1.porat@gmail.com>
2024-03-08 14:05:20 +0200

    ui changes

--- frontend/src/components/manegerComponents/ManagerHomePage.js
+++ frontend/src/components/manegerComponents/ManagerHomePage.js
@@ -77,7 +77,7 @@ const ManagerHomePage = () => {
     };
 
     return <ManagerContext.Provider value={{getUser}}>
-        <PageLayout className={styles.navbar} text={`ברוך הבא, ${fullname}`} noAnimation={true}>
+        <PageLayout className={styles.navbar} text={`שלום, ${fullname}`} noAnimation={true}>
             
             <div className={styles.container}>
 
